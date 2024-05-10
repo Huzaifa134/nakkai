57aee99b4608ce1a41be22fb456074fced978f58
Muhammad Huzaifa <mhuzaifa134@gmail.com>
2024-05-10 17:19:29 +0500

    category update

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -146,7 +146,7 @@ const HeroBanner = () => {
 
                   <ul className="mt-2 space-y-1 px-4">
                     {categories.map((category) => (
-                      <li key={category} onClick={() => handleClick(category)}>
+                      <li key={category} >
                         <details className="group [&_summary::-webkit-details-marker]:hidden">
                           <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
                             <Link
